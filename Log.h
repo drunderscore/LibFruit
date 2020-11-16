6c11a0ae5359e31db40f9fdf943e03abc4ffa849
James Puleo <electricman226@gmail.com>
2020-11-16 00:55:15 -0500

    Log: Remove loguru in favor of our own, simpiler, logging interface

--- Log.h
+++ Log.h
@@ -15,9 +15,45 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #pragma once
+#include <stdio.h>
+#include "fmt/core.h"
 
-// TODO: Integrate more logging libraries, and have a common
-// interface for all of them.
-// For now, loguru it is!
+namespace LibFruit
+{
+    class Logger
+    {
+    public:
+        Logger(FILE* handle) : m_handle(handle) {}
 
-#include "loguru/loguru.hpp"
+        template<typename... TArgs>
+        void println(const char* msg, TArgs&&... args)
+        {
+            fmt::print(m_handle, msg, args...);
+            fmt::print(m_handle, "\n");
+        }
+
+        static Logger& out() { return m_out; }
+        static Logger& error() { return m_err; }
+
+    private:
+        static Logger m_out;
+        static Logger m_err;
+        FILE* m_handle;
+    };
+}
+using LibFruit::Logger;
+
+// Macros are ugly, but we need them for logging compile-time data.
+#define FRUIT_STRINGIFY_(x) #x
+#define FRUIT_STRINGIFY(x) FRUIT_STRINGIFY_(x)
+#define CHECK(condition) CHECK_MSG(condition, "???")
+#define CHECK_MSG(condition, msg, ...) \
+if(!(condition)) \
+{ \
+    LibFruit::Logger::error().println("Assertion failed: " FRUIT_STRINGIFY(condition)); \
+    LibFruit::Logger::error().println("\tWhat: {}", fmt::format(msg, __VA_ARGS__) ); \
+    LibFruit::Logger::error().println("\tFile: " FRUIT_STRINGIFY(__FILE__)); \
+    LibFruit::Logger::error().println("\tFunction: " __FUNCTION__); \
+    LibFruit::Logger::error().println("\tLine: " FRUIT_STRINGIFY(__LINE__)); \
+    abort(); \
+}
